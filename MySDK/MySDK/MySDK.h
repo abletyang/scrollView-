c7b177d91a76b55dce02fd5f50480d4ce96213a3
yangyun <yangyun@tzt.cn>
2018-06-30 13:53:35 +0800

    添加 MySDK

---
+++ MySDK/MySDK/MySDK.h
@@ -0,0 +1,15 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by 杨赟 on 2018/6/30.
+//  Copyright © 2018年 杨赟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MySDK : NSObject
+
++(void)sayhello;
+
+@end
---
+++ MySDK/MySDK/MySDK.m
@@ -0,0 +1,18 @@
+//
+//  MySDK.m
+//  MySDK
+//
+//  Created by 杨赟 on 2018/6/30.
+//  Copyright © 2018年 杨赟. All rights reserved.
+//
+
+#import "MySDK.h"
+
+@implementation MySDK
+
++(void)sayhello
+{
+    NSLog(@"我是 SDK");
+}
+
+@end
